b096c52623ecf971d2ea8aaec3e24550d83ce01a
Paul Keeler <40744936+hpaulkeeler@users.noreply.github.com>
2018-10-09 11:17:06 +0200

    Update DemoDetPoisson.py

--- DemoDetPoisson.py
+++ DemoDetPoisson.py
@@ -2,27 +2,22 @@
 #
 # A determinantally-thinned Poisson point process is essentially a discrete
 # determinantal point process whose underlying state space is a single 
-# realization of a Poisson point process defined on some bounded continuous 
-# space. 
+# realization of a Poisson point process defined on some bounded continuous space. 
 #
 # For more details, see the paper by Blaszczyszyn and Keeler[1].
 #
-# Author: H.P. Keeler, Inria/ENS, Paris, and University of Melbourne, 
-# Melbourne, 2018.
+# Author: H.P. Keeler, Inria/ENS, Paris, and University of Melbourne, Melbourne, 2018.
 #
 #References:
-#[1] Blaszczyszyn and Keeler, Determinantal thinning of point processes with 
-#network learning applications, 2018.
+#[1] Blaszczyszyn and Keeler, Determinantal thinning of point processes with network learning applications, 2018.
 
-#import relevant libraries
-import numpy as np
-from scipy.linalg import orth
-import scipy.stats
-import matplotlib.pyplot as plt
+rm(list=ls(all=TRUE)); #clear all variables
+graphics.off(); #close all figures
+cat("\014"); #clear screen
 
 #START -- Parameters -- START
 #Poisson point process parameters
-lambda0=50; #intensity (ie mean density) of the Poisson process
+lambda=50; #intensity (ie mean density) of the Poisson process
 
 #choose kernel
 choiceKernel=2;#1 for Gaussian (ie squared exponetial ); 2 for Cauchy
@@ -35,66 +30,79 @@ xDelta=xMax-xMin;yDelta=yMax-yMin; #rectangle dimensions
 areaTotal=xDelta*yDelta; #area of rectangle
 #END -- Parameters -- END
 
-#Simulate a Poisson point process
-numbPoints = scipy.stats.poisson(lambda0*areaTotal).rvs();#Poisson number of points
-xx = scipy.stats.uniform.rvs(0,xDelta,((numbPoints,1)))+xMin;#x coordinates of Poisson points
-yy = scipy.stats.uniform.rvs(0,yDelta,((numbPoints,1)))+yMin;#y coordinates of Poisson points
+#Simulate a Poisson point process on a rectangle
+numbPoints=rpois(1,areaTotal*lambda);#Poisson number of points
+xx=xDelta*runif(numbPoints)+xMin;#x coordinates of Poisson points
+yy=xDelta*runif(numbPoints)+yMin;#y coordinates of Poisson points
 
 # START -- CREATE L matrix -- START 
 sizeL=numbPoints;
 #Calculate Gaussian or kernel kernel based on grid x/y values
 #all squared distances of x/y difference pairs
-xxDiff=np.outer(xx, np.ones((numbPoints,)))-np.outer( np.ones((numbPoints,)),xx);
-yyDiff=np.outer(yy, np.ones((numbPoints,)))-np.outer( np.ones((numbPoints,)),yy)
-rrDiffSquared=(xxDiff**2+yyDiff**2);
-
-if choiceKernel==1:
-    #Gaussian/squared exponential kernel
-    L=lambda0*np.exp(-(rrDiffSquared)/sigma**2);
-else:
-    if choiceKernel==2:
-        #Cauchy kernel
-        L=lambda0/(1+rrDiffSquared/sigma**2)**(alpha+1/2);    
-    else:        
-        raise Exception('choiceKernel has to be equal to 1 or 2.');
-     
+xxDiff=(outer(xx,rep(1,sizeL))-outer(rep(1,sizeL),xx));
+yyDiff=(outer(yy,rep(1,sizeL))-outer(rep(1,sizeL),yy));
+rrDiffSquared=(xxDiff^2+yyDiff^2);
+if (choiceKernel==1){
+  #Gaussian/squared exponential kernel
+  L=lambda*exp(-(rrDiffSquared)/sigma^2);
+} else{ 
+  if (choiceKernel==2){
+    #Cauchy kernel
+    L=lambda/(1+rrDiffSquared/sigma^2)^(alpha+1/2);
+  } else{
+    stop('choiceKernel has to be equal to 1 or 2.');
+  }
+}
 # END-- CREATE L matrix -- # END
 
 # START Simulating/sampling DPP
 #Eigen decomposition
-eigenValuesL, eigenVectorsL=np.linalg.eig(L);
-eigenValuesK = eigenValuesL / (1+eigenValuesL); #eigenvalues of K
-indexEig = (np.random.rand(sizeL) < eigenValuesK );#Bernoulli trials
+tmp=eigen(L); eigenValuesL=tmp$values; eigenVectorsL=tmp$vectors
+eigenVectorsL[,2]=tmp$vectors[,3]; eigenVectorsL[,3]=tmp$vectors[,2]# REMOVE later
+
+eigenValuesK <- eigenValuesL / (1+eigenValuesL); #eigenvalues of K
+indexEigen <- which(runif(sizeL) <= eigenValuesK); #Bernoulli trials
 
 #number of points in the DPP realization
-numbPointsDPP= np.sum(indexEig);  #number of points 
+numbPointsDPP<-length(indexEigen);
 #retrieve eigenvectors corresponding to successful Bernoulli trials
-spaceV = eigenVectorsL[:, indexEig]; #subspace V
-indexDPP=list(); #list for index final DPP configuration
+spaceV <- eigenVectorsL[,indexEigen]; #subspace V
+indexDPP <- rep(0,numbPointsDPP,1); #vector for final DPP configuration
 
 #Loop through for all points
-for ii in range(numbPointsDPP):
-    #Compute probabilities for each point i    
-    Prob_i = np.sum(spaceV**2, axis=1);#sum across rows
-    Prob_i = np.cumsum(Prob_i/ np.sum(Prob_i)); #normalize
+if (numbPointsDPP>1){
+  for (ii in numbPointsDPP:1){
+    #Compute probabilities for each point i
+    Prob_i <- rowSums(spaceV^2); #sum across rows
+    Prob_i <- Prob_i / sum(Prob_i); #normalize
     
-    #Choose a new point using PMF Prob_i  
-    indexCurrent=(np.random.rand() <= Prob_i).argmax();
-    indexDPP.append(indexCurrent);    
+    #Choose a new point using PMF Prob_i
+    indexCurrent <- min(which(cumsum(Prob_i)>runif(1)));
+    indexDPP[ii]<-indexCurrent;
     
     #Choose a vector to eliminate
-    jj = (np.abs(spaceV[indexCurrent, :]) > 0).argmax() 
-    columnVj = spaceV[:, jj];
+    jj<-min(which(spaceV[indexCurrent,]!=0));    
+    columnVj <- spaceV[,jj];
+    spaceV <- spaceV[,-jj];
+    
+    spaceV=matrix(spaceV,sizeL,ii-1); #reshape matrix
     
     #Update matrix V
-    spaceV = orth(spaceV- (np.outer(columnVj,(spaceV[indexCurrent, :] / columnVj[indexCurrent])))); 
-   
-indexDPP.sort(); #sort points
+    spaceV <- spaceV - outer(columnVj,(spaceV[indexCurrent,]/columnVj[indexCurrent])); #remove Vj component from the space
+    #subspaceV<-orth(subspaceV); # Orthogonalize V using SVD
+    tempQR=qr(spaceV,k=ii);
+    spaceV<-qr.Q(tempQR); #Orthonormalize using Householder method
+    
+  }
+}
+indexDPP <- sort(indexDPP); #sort points
+indexDPP <-indexDPP;
 #END - Simulating/sampling DPP - END
 
-#Plotting 
-plt.scatter(xx,yy, edgecolor='k', facecolor='none');
-plt.xlabel("x"); plt.ylabel("y");
-#random color vector
-vectorColor=(np.asscalar(np.random.rand(1)), np.asscalar(np.random.rand(1)), np.asscalar(np.random.rand(1)));
-plt.scatter(xx[indexDPP],yy[indexDPP],edgecolor='none',facecolor=vectorColor);
+#Plotting
+#Plot Poisson point process
+plot(xx,yy,col="black",pch=1,cex=3);
+colorAll=colors(); #list all colors
+colorRand=colorAll[sample(length(colorAll),1)] #randomly choose one color
+#Plot determinantally-thinned Poisson point process
+points(xx[indexDPP],yy[indexDPP],col=colorRand,pch=16,cex=2);
