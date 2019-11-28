14eaead2e2ab595a2d0f6c1b0e09c5166b6e15d6
Paul Keeler <hpkeeler@gmail.com>
2019-11-28 11:10:17 +0100

    Update funLtoK.py

--- funLtoK.py
+++ funLtoK.py
@@ -1,6 +1,6 @@
 # K=funLtoK(L)
-# The function funLtoK(L) converts a kernel L matrix into  a (normalized) 
-# kernel K matrix. The K matrix has to be semi-positive definite.
+# The function funLtoK(L) converts a (non-singular) kernel L matrix into  a (normalized) 
+# kernel K matrix. The L matrix has to be semi-positive definite.
 
 import numpy as np #NumPy package for arrays, random number generation, etc
 
@@ -12,8 +12,15 @@ def funLtoK(L):
     K=np.real(K); #make sure all values are real        
     return K
     
-
-#K=funLtoK(L)
+# TEST 
+# B=np.array([[3, 2, 1],[4, 5,6],[ 9, 8,7]])
+# A=np.matmul(B.transpose(),B)
+# A= 
+# array([[3, 4, 9],
+#        [2, 5, 8],
+#        [1, 6, 7]])
+#
+# K=funLtoK(L)
 # K=
 #array([[ 0.76022099,  0.33480663, -0.09060773],
 #       [ 0.33480663,  0.3320442 ,  0.32928177],
